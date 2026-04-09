8a4728981fa4f5eef36909105b8378a51a11b2b9
Jiung-start <inghappysss1689@gmail.com>
2026-04-09 19:38:07 +0900

    feat: 실습4

--- week1/양지웅/src/week1Pc/User.java
+++ week1/양지웅/src/week1Pc/User.java
@@ -1,19 +1,45 @@
 package week1Pc;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class User {
-    String name;
-    int age;
+   private String name;
+   private int age;
 
     public User(String name, int age ) {
         this.name = name;
         this.age = age;
     }
+    String getName() {
+        return name;
+    }
+    int getAge() {
+        return age;
+    }
+    void setName(String name) {
+        this.name = name;
+    }
+    void setAge(int age) {
+        if(age<0) {
+            System.out.println("올바르지 않은 나이입니다.");
+
+        }
+        this.age = age;
+    }
+
+
     public void printInfo() {
         System.out.println("name "+ name + " age "+ age);
     }
     public static void main(String[] args) {
         User user1 = new User("지웅", 20);
 
+
+
+        user1.setAge(25);
+
         user1.printInfo();
+
+
     }
 }
