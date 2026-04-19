f1872dbc44e230ee683b2cd4c4b95f6e19a150ca
Hwa Young Yang <110398814+sanchaehwa@users.noreply.github.com>
2026-04-19 16:58:35 +0900

    feat: Post 엔티티 설계

---
+++ src/main/java/com/post/domain/post/entity/Post.java
@@ -0,0 +1,54 @@
+package com.post.domain.post.entity;
+
+import com.post.global.domain.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Builder
+public class Post extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "post_id")
+	private Long postId;
+
+	@Column(nullable = false, length = 100)
+	private String title;
+
+	@Column(nullable = false, columnDefinition = "TEXT")
+	private String content;
+
+	@Column(nullable = false)
+	private String author;
+
+	public static Post create(String title, String content, String author) {
+		return Post.builder()
+			.title(title)
+			.content(content)
+			.author(author)
+			.build();
+	}
+
+	//업데이트 로직
+	public void update(String title, String content, String author) {
+		this.title = title;
+		this.content = content;
+		this.author = author;
+	}
+
+}
